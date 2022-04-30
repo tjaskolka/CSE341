c17c66254bedc683997d39d4873037cf80db59cc
Terri Jaskolka <jas20003@byui.edu>
2022-04-30 16:47:50 -0400

    added rest

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,24 @@
 const routes = require('express').Router();
 const connection = require('../db/connection');
+const ObjectId = require('mongodb').ObjectId;
+
+routes.get('/:id', (req, res) =>{
+
+    const contactId = new ObjectId(req.params.id);
+    const results = connection.getCollection().find({_id: contactId});
+
+    results.toArray().then((documents) => {
+        res.status(200).json(documents[0]);
+        console.log(`Returned contact ${req.params.id}`);
+    });
+});
 
 routes.get('/', (req, res) =>{
 
-    connection.getCollection().find().toArray((err, result) => {
-        if (err) throw err;
-        res.json(result);
-        console.log("Contacts query made");
-//        db. close();
+    const results = connection.getCollection().find();
+    results.toArray().then((documents) => {
+        res.status(200).json(documents);
+        console.log("Returned all contacts");
     });
 });
 
