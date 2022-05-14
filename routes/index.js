44e83f05a1db8f7fedaab074d1280b7331502397
Terri Jaskolka <jas20003@byui.edu>
2022-05-14 11:42:24 -0400

    added root route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
+router.use('/', require('./contacts'));
 
 module.exports = router;
